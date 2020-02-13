c1837dfa34e5b73ea93b52ae10c22a64e990646d
ddebutts <dan.debutts@gmail.com>
2020-02-13 10:20:44 -0500

    3rd time a charm?

---
+++ public/webpack.config.js
@@ -0,0 +1,59 @@
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: {
+    founders: './assets/js/founders.js',
+    hall: './assets/js/hall.js',
+    holcard: './assets/js/holcard.js',
+    index: './assets/js/index.js',
+    ipsum: './assets/js/ipsum.js',
+    larry_defined: './assets/js/larry_defined.js',
+    script: './assets/js/script.js',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'my-domain-cache-id',
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/],
+    }),
+    // new WebpackPwaManifest({
+    //   name: 'Newsy app',
+    //   short_name: 'Newsy',
+    //   description: 'An application that allows you to view different news articles and save your favorites.',
+    //   background_color: '#01579b',
+    //   theme_color: '#ffffff',
+    //   'theme-color': '#ffffff',
+    //   start_url: '/',
+    //   icons: [{
+    //     src: path.resolve('assets/images/icons/icon192x192.png'),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join('assets', 'icons'),
+    //   }],
+    // }),
+  ],
+};
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const db = require('./models');
+const routes = require('./routes');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+// Sync sequelize models then start Express app
+// =============================================
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT} and process is PID ${process.pid}`);
+  });
+});
+
